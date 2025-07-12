a01f52dc4e1d59ece5d91e854ec7c638c42d3c1a
gh2543 <gh2543@nyu.edu>
2025-07-12 13:29:58 -0400

    Create subnet.py

---
+++ subnet.py
@@ -0,0 +1,75 @@
+#enter your unique code
+uniquecode = 37509
+
+#problem 1
+p1Network = '195.122.115.0'
+p1Broadcast = '95.122.115.31'
+p1First = '195.122.115.1'
+p1Last =  '95.122.115.30'
+
+#problem 2
+p2Network = '106.107.76.0'
+p2Broadcast = '106.107.76.255'
+p2First = '106.107.76.0'
+p2Last = '106.107.76.254'
+
+#problem 3
+p3Network = ''
+p3Broadcast = ''
+p3First = ''
+p3Last = ''
+
+#problem 4
+p4Network = ''
+p4Broadcast = ''
+p4First = ''
+p4Last = ''
+
+#problem 5
+p5Network = ''
+p5Broadcast = ''
+p5First = ''
+p5Last = ''
+
+#problem 6
+p6Network = ''
+p6Broadcast = ''
+p6First = ''
+p6Last =  ''
+
+#problem 7
+p7Network = ''
+p7Broadcast = ''
+p7First = ''
+p7Last = ''
+
+#problem 8
+p8Network = ''
+p8Broadcast = ''
+p8First = ''
+p8Last = ''
+
+#problem 9
+p9Network = ''
+p9Broadcast = ''
+p9First = ''
+p9Last = ''
+
+#problem 10
+p10Network = ''
+p10Broadcast = ''
+p10First = ''
+p10Last = ''
+
+
+"""
+example:
+
+uniquecode = 12345
+
+problem 1
+p1Network = '199.204.14.128'
+p1Broadcast = '199.204.14.255'
+p1First = '199.204.14.129'
+p1Last =  '199.204.14.254'
+"""
